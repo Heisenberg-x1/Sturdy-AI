02e846058e5da786e4dee0d123b19e27c7f44ca8
bug_101 <chemerilcollins@gmail.com>
2023-01-29 23:15:06 +0100

    Commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let nav = document.querySelector(".nav")
 let div1 = document.querySelector(".div1")
+let b = document.querySelector(".container")
 nav.addEventListener("click",function () {
     
 
@@ -7,7 +8,8 @@ nav.addEventListener("click",function () {
     div1.style.animationDuration = "1s"
     div1.style.animationTimeFunction = "ease-in-out"
     div1.style.animationFillMode = "forwards"
-    div1.style.transform = "rotateY(-50deg) translateX(200px)";
+    div1.style.transform = "rotateY(-50deg) translateX(100px)";
+    b.style.background= "rgb(49, 41, 23)"
 
 
 })
