54d9f15dce6c17f4d717687a2c402936b6aa095e
bug_101 <chemerilcollins@gmail.com>
2023-01-29 01:01:25 +0100

    Commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let nav = document.querySelector(".navig")
+let nav = document.querySelector(".nav")
 let div1 = document.querySelector(".div1")
 nav.addEventListener("click",function () {
+    
+
     div1.style.animationName = "mine"
-    div1.style.animationDuration = "2s"
-    div1.style.animationTimeFunction = "ease"
+    div1.style.animationDuration = "1s"
+    div1.style.animationTimeFunction = "ease-in-out"
     div1.style.animationFillMode = "forwards"
-    div1.style.transform = "rotateY(-10 deg)"
-    div1.style.perspective = "1000px"
-    div1.style.transformStyle = "preserve-3d "
-    document.body.style.transformStyle = "preserve-3d "
-    document.body.style.perspective = "1000px"
+    div1.style.transform = "rotateY(-50deg) translateX(200px)";
+
+
 })
